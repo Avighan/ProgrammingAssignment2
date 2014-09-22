5595af9fcfdb058025b3effff5645ce06432513c
Avighan <dpn.majumder@gmail.com>
2014-09-22 12:06:46 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,27 +1,26 @@
 #Cache matrix vector
-makeCacheMatrix <- function(x = numeric()){
-m <- NULL
-set <- function(y){
+makeCacheMatrix <- function(x = matrix()){
+  m <- NULL
+  set <- function(y){
     x <<- y
     m <<- NULL
-}
-get <- function() x
-setinverse <- function(solve) m <<- solve(m)
-getinverse <- function() m
-list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve(m)
+  getinverse <- function() m
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
 #function calling the cache data, if not present recalculate else pull the same data
 cacheSolve <- function(x, ...) {
-        m <- x$getinverse()
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
-        }
-        data <- x$get()
-        m <- solve(data, ...)
-        x$setinverse(m)
-        m
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
-
